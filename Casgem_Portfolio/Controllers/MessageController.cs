974bac98339097182bbd53a1b5ff7dc5a768b0b2
hakanbahsis <hakan.bahsis@hotmail.com>
2023-07-10 16:23:12 +0300

    first commit

---
+++ Casgem_Portfolio/Controllers/MessageController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Casgem_Portfolio.Models;
+namespace Casgem_Portfolio.Controllers
+{
+    public class MessageController : Controller
+    {
+        CasgemPortfolioEntities1 db = new CasgemPortfolioEntities1();
+        // GET: Message
+        public ActionResult Index()
+        {
+            var values = db.TblMessage.ToList();
+            return View(values);
+        }
+
+        public ActionResult DeleteMessage(int id)
+        {
+            var values = db.TblMessage.Find(id);
+            db.TblMessage.Remove(values);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult MessageDetails(int id)
+        {
+            var values = db.TblMessage.Find(id);
+            return View(values);
+        }
+    }
+}
---
+++ Casgem_Portfolio/Controllers/ServiceController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Casgem_Portfolio.Models;
+
+namespace Casgem_Portfolio.Controllers
+{
+    public class ServiceController : Controller
+    {
+        CasgemPortfolioEntities1 db = new CasgemPortfolioEntities1();
+        // GET: Service
+        public ActionResult Index()
+        {
+            var values = db.TblServices.ToList();
+            return View(values);
+        }
+
+        public ActionResult DeleteService(int id)
+        {
+            var values = db.TblServices.Find(id);
+            db.TblServices.Remove(values);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult AddService()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddService(TblServices services)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TblServices.Add(services);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(services);
+        }
+
+        [HttpGet]
+        public ActionResult UpdateService(int id)
+        {
+            var values = db.TblServices.Find(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateService(TblServices services)
+        {
+            if (ModelState.IsValid)
+            {
+                var values = db.TblServices.Find(services.ServicesId);
+                values.ServiceTitle = services.ServiceTitle;
+                values.Icon = services.Icon;
+                values.ServiceContent = services.ServiceContent;
+                values.ServiceNumber = services.ServiceNumber;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
